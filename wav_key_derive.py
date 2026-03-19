c7cf53ef3d8239a38c8698373977e0a518f07066
teg0023 <127151198+teg0023@users.noreply.github.com>
2026-03-19 17:40:22 -0500

    Added a parameter for the number of frames in a WAV file and added the if statement to handle hex values having less than two characters.

--- wav_key_derive.py
+++ wav_key_derive.py
@@ -2,11 +2,12 @@ import wave
 """
 This module consists of the method to derive the key from the wav file.
 """
-def wav_key_derive(wav_file : str):
+def wav_key_derive(wav_file : str, nbr_audio_frames : int):
     """
     This method derives a secret symmetric key from the audio frames of the WAV file.
 
     :param wav_file (str): WAV file name
+    :param nbr_audio_frames (int): number of audio frames in a WAV file
     :return: The key as string
     """
     if not wav_file.endswith('.wav'):
@@ -14,13 +15,15 @@ def wav_key_derive(wav_file : str):
 
     """Opening and reading the raw data of the WAV file"""
     with wave.open(wav_file, 'rb') as w:
-        raw_data = w.readframes(w.getnframes())
+        raw_data = w.readframes(nbr_audio_frames)
 
     """Reformatting the raw data of the WAV file into a string of hex values."""
     data = str(raw_data).removeprefix('b\'').removesuffix('\'')
     data = data.split('\\x')
     key = ''
     for i in range(1, len(data)):
-        key += '\\x' + data[i][0] + data[i][1]
+        """If the hex value has less than 2 characters, skip it."""
+        if len(data[i]) > 1:
+            key += '\\x' + data[i][0] + data[i][1]
 
     return key
