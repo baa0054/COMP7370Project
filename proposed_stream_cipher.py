32042953f98681f6d4d24a6fad1e98959e3cc7be
teg0023 <127151198+teg0023@users.noreply.github.com>
2026-03-26 21:53:38 -0500

    Made a modification on the cipher's encryption and decryption (will be based on ASCII).

--- proposed_stream_cipher.py
+++ proposed_stream_cipher.py
@@ -1,9 +1,7 @@
-import random
 """
 This Python module consists of encryption and decryption methods of the proposed stream cipher.
 """
-# The key derived from the WAV file.
-wav_derived_key : str
+
 def proposed_stream_cipher_encrypt(plaintext : str, key : str):
     """
     This function performs simple stream cipher encryption.
@@ -11,57 +9,47 @@ def proposed_stream_cipher_encrypt(plaintext : str, key : str):
     :param key (str): The key derived from the WAV file as a hex value.
     :return: The ciphertext as string
     """
-    # Encoding the plaintext via UTF-8.
-    plaintext_hex = '\\x' + plaintext.encode('utf-8').hex(' ').replace(' ','\\x')
+    if len(plaintext) == 0:
+        return ""
+    # Encoding the plaintext via ASCII.
+    plaintext_hex = '\\x' + plaintext.encode('ascii').hex(' ').replace(' ','\\x')
 
     # Splitting the plaintext and key hex bytes by their delimiter '\x' creating lists of them respectively.
     plaintext_list = plaintext_hex.split('\\x')
     key_list = key.split('\\x')
+    plaintext_list.remove('')
+    key_list.remove('')
 
     # Perform XOR operation between the plaintext hex value and key hex value byte by byte.
-    ciphertext_hex = ""
-    padding = ""
+    ciphertext = ""
     for i in range(len(plaintext_list)):
-        if i < len(key_list):
-            ciphertext_hex += ('' + format(int(plaintext_list[i][0], 16) ^ int(key_list[i][0], 16), 'x')
-                                  + format(int(plaintext_list[i][1], 16) ^ int(key_list[i][1], 16), 'x'))
-        else:
-            # Padding of random hex bytes if key's length is less than the plaintext length.
-            h1 = random.randrange(0, 15)
-            h2 = random.randrange(0, 15)
-            padding += '\\x' + format(h1, 'x') + format(h2, 'x')
-            ciphertext_hex += ('' + format(int(plaintext_list[i][0], 16) ^ h1, 'x')
-                                  + format(int(plaintext_list[i][1], 16) ^ h2, 'x'))
-    # Add padding to the key.
-    key += padding
-    wav_derive_key = key
+        ciphertext_hex = ('' + format(int(plaintext_list[i], 16) ^ int(key_list[i], 16), 'x'))
+        ciphertext += ''.join([chr(int(ciphertext_hex[i:i + 2], 16)) for i in range(0, len(ciphertext_hex), 2)])
 
-    # Decoding the ciphertext via UTF-8 and then return it.
-    ciphertext = ''.join([chr(int(ciphertext_hex[i:i + 2], 16)) for i in range(0, len(ciphertext_hex), 2)])
     return ciphertext
 
 def proposed_stream_cipher_decrypt(ciphertext : str, key : str):
     """
         This function performs simple stream cipher decryption.
         :param ciphertext (str): The ciphertext
-        :param key (str): The key derived from the WAV file as a hex value (it may be extended with padding
-                          from encryption).
+        :param key (str): The key derived from the WAV file as a hex value.
         :return: The plaintext as string
     """
-    # Encoding the ciphertext via UTF-8.
-    ciphertext_hex = '\\x' + ciphertext.encode('utf-8').hex(' ').replace(' ', '\\x')
+    if len(ciphertext) == 0:
+        return ""
+    # Encoding the ciphertext via ASCII.
+    ciphertext_hex = '\\x' + ciphertext.encode('ascii').hex(' ').replace(' ', '\\x')
 
     # Splitting the ciphertext and key hex values.
     ciphertext_list = ciphertext_hex.split('\\x')
     key_list = key.split('\\x')
-
+    ciphertext_list.remove('')
+    key_list.remove('')
 
     # Perform XOR operations between the plaintext hex value and key hex value byte by byte.
-    plaintext_hex = ""
+    plaintext = ""
     for i in range(len(ciphertext_list)):
-        plaintext_hex += ('' + format(int(ciphertext_list[i][0], 16) ^ int(key_list[i][0], 16), 'x')
-                             + format(int(ciphertext_list[i][1], 16) ^ int(key_list[i][1], 16), 'x'))
+        plaintext_hex = ('' + format(int(ciphertext_list[i], 16) ^ int(key_list[i], 16), 'x'))
+        plaintext += ''.join([chr(int(plaintext_hex[i:i + 2], 16)) for i in range(0, len(plaintext_hex), 2)])
 
-    # Decoding the plaintext via UTF-8 and then return it.
-    plaintext = ''.join([chr(int(plaintext_hex[i:i + 2], 16)) for i in range(0, len(plaintext_hex), 2)])
     return plaintext
