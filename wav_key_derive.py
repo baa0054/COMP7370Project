b3ccb8eed8bd211de35bf7d190a0a2400c12e9b7
teg0023 <127151198+teg0023@users.noreply.github.com>
2026-03-26 21:50:32 -0500

    Made a modification on the WAV File Key Derivation function to implement padding of random ASCII hex bytes if the key length is less than the plaintext length (as numbers of hex bytes respective).

--- wav_key_derive.py
+++ wav_key_derive.py
@@ -1,13 +1,15 @@
 import wave
+import random
 """
 This module consists of the method to derive the key from the wav file.
 """
-def wav_key_derive(wav_file : str, nbr_audio_frames : int):
+def wav_key_derive(wav_file : str, nbr_audio_frames : int, plaintext : str = ""):
     """
     This method derives a secret symmetric key from the audio frames of the WAV file.
 
     :param wav_file (str): WAV file name
     :param nbr_audio_frames (int): number of audio frames in a WAV file
+    :param plaintext (str, optional): The plaintext
     :return: The key as string
     """
     if not wav_file.endswith('.wav'):
@@ -24,6 +26,25 @@ def wav_key_derive(wav_file : str, nbr_audio_frames : int):
     for i in range(1, len(data)):
         """If the hex value has less than 2 characters, skip it."""
         if len(data[i]) > 1:
-            key += '\\x' + data[i][0] + data[i][1]
+            key += '\\x' + format(int(data[i][0], 16) % 8, 'x') + format(int(data[i][1], 16) % 16, 'x')
 
+    if len(plaintext) != 0:
+        plaintext_hex = '\\x' + plaintext.encode('ascii').hex(' ').replace(' ', '\\x')
+
+        kh = key.split('\\x')
+        ph = plaintext_hex.split('\\x')
+        kh.remove('')
+        ph.remove('')
+
+        """
+        If the key length is less than the plaintext length (in number of hex bytes), implement random ASCII hex byte 
+        padding.
+        """
+        if len(kh) < len(ph):
+            d = len(ph) - len(kh)
+            for i in range(d):
+                h1 = random.randint(0, 7)
+                h2 = random.randint(0, 15)
+                h3 = '' + format(int(h1), 'x') + format(int(h2), 'x')
+                key += '\\x' + h3
     return key
