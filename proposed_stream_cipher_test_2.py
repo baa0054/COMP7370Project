2cfdb1144685d1454d3cd8a5a09cd582720861f3
baa0054 <baa0054@auburn.edu>
2026-04-01 18:49:02 -0500

    Update proposed_stream_cipher_test_2.py
    Updating the test

--- proposed_stream_cipher_test_2.py
+++ proposed_stream_cipher_test_2.py
@@ -2,12 +2,15 @@ import proposed_stream_cipher as psc
 import wav_key_derive as w
 import entropy_test as et
 import plaintextAttack as pta
+import replay as re
+
 
 #print out padding
 FILE_W = 25
 TEXT_W = 25
 STAT_W = 10
 PTA_W  = 15
+PRE_W = 10
 
 """
 This Python script tests encryption and decryption of multiple plaintexts.
@@ -29,30 +32,35 @@ def proposed_stream_cipher_test_2():
         for filename in files:
             print(f"\n\tWAV File: {filename}")
             print("\tEncryption")
-            wav_derived_key = w.wav_key_derive(filename, NUMBER_OF_FRAMES, p)
+            formatted_key = w.wav_key_derive(filename, NUMBER_OF_FRAMES, p)
             #Store and print test results
-            testResults = et.randomness_simulations(wav_derived_key, filename, p)
+            testResults = et.randomness_simulations(formatted_key, filename, p)
+            replayandpredict = re.replayAndPredict(w.wav_key_derive, filename, p)
+            ciphertext = psc.proposed_stream_cipher_encrypt(p, formatted_key)
 
-            ciphertext = psc.proposed_stream_cipher_encrypt(p, wav_derived_key)
-            
             recoveredBytes = pta.extractKey(p, ciphertext)
             recoveredKey = pta.lookForKeyLoop(recoveredBytes)
-            testResults['PTA'] = recoveredKey if recoveredKey else "None"
+            testResults.update({
+                "replay": replayandpredict['replay'],
+                "predict": replayandpredict['predictability'],
+                "pta": recoveredKey
+            })
             simResults.append(testResults)
-            
             print(f"\tCiphertext: {ciphertext}")
             print("\tDecryption")
-            plaintext = psc.proposed_stream_cipher_decrypt(ciphertext, wav_derived_key)
+            plaintext = psc.proposed_stream_cipher_decrypt(ciphertext, formatted_key)
             print(f"\tPlaintext After Decryption: {plaintext}")
         print("----------------------------------------------------------------------------------------------")
         print("----------------------------------------------------------------------------------------------")
+
+
     #Shows the test results
     print("Simulation Report\n")
-    header = f"{'WAV File':<{FILE_W}} | {'Plaintext':<{TEXT_W}} | {'Status':<{STAT_W}} | {'PTA (Period)'}"
+    header = f"{'WAV File':<{FILE_W}} | {'Plaintext':<{TEXT_W}} | {'Status':<{STAT_W}} | {'PTA (Period)':<{PTA_W}} | {'Replay':<{PRE_W}} | {'Predictability':<{PRE_W}}"
     print(header)
     for res in simResults:
             display = ((res['text'][:TEXT_W-3] + "...") if len(res['text']) > TEXT_W else res['text'])
-            print(f"{res['file']:<{FILE_W}} | "f"{display:<{TEXT_W}} | "f"{str(res['status']):<{STAT_W}} | "f"{str(res['PTA']):<{PTA_W}}")
+            print(f"{res['file']:<{FILE_W}} | "f"{display:<{TEXT_W}} | "f"{str(res['status']):<{STAT_W}} | "f"{str(res['pta']):<{PTA_W}} | "f"{str(res['replay']):<{PRE_W}} | "f"{str(res['predict']):<{PRE_W}}")
         
 if __name__ == "__main__":
     proposed_stream_cipher_test_2()
