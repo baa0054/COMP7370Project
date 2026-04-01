9eabe483292dff899c65f1ea0ad97d4563e8e5d9
baa0054 <baa0054@auburn.edu>
2026-03-31 20:35:00 -0500

    Update plaintextAttack.py
    Now used in PSC test 1 and 2

--- plaintextAttack.py
+++ plaintextAttack.py
@@ -7,18 +7,7 @@ def extractKey(plaintext, ciphertext):
 
 def lookForKeyLoop(recoveredKey):
     length = len(recoveredKey)
-    for loop in range(1, length // 2):
+    for loop in range(1, length // 2 + 1):
         if recoveredKey[:loop] == recoveredKey[loop:2 * loop]:
-            return loop
+            return f"Loop found: {loop}"
     return None
-
-
-
-if __name__ == "__main__":
-    plaintext = """This is the best day ever, and I am really having a good time today."""
-    ciphertext = r"""),c!BjXv7JO,/6e`{7>qn[z*R56GX2%Jc$I2Sx$dnY49g"=Z&tV~w\W0;~"""
-    recovered_key = extractKey(plaintext, ciphertext)
-    loop_size = lookForKeyLoop(recovered_key)
-
-    print(f"Key Period Found: {loop_size}")
-    print(f"Recovered Key (first few bytes): {recovered_key[:10]}")
