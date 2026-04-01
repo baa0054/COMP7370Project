a967fb194cb1eb99339991b1b80511c2c8e56324
baa0054 <baa0054@auburn.edu>
2026-03-31 20:47:40 -0500

    Update plaintextAttack.py
    Forgot to add the comments. My bad

--- plaintextAttack.py
+++ plaintextAttack.py
@@ -1,3 +1,4 @@
+#Trys to find the key from the ciphertext and plaintext
 def extractKey(plaintext, ciphertext):
     plainttextHex = [int(pByte) for pByte in plaintext.encode("ascii")]
     ciphertextHex = [int(cByte) for cByte in ciphertext.encode("ascii")]
@@ -5,6 +6,7 @@ def extractKey(plaintext, ciphertext):
     recoveredKey = [plain ^ cipher for plain, cipher in zip(plainttextHex, ciphertextHex)]
     return recoveredKey
 
+# Looks for loops in the ciphertext and plaintext
 def lookForKeyLoop(recoveredKey):
     length = len(recoveredKey)
     for loop in range(1, length // 2 + 1):
