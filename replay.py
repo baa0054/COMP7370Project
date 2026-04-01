c1f53295518dc33994632b164519ac2894640fe2
baa0054 <baa0054@auburn.edu>
2026-03-31 23:31:49 -0500

    Update replay.py
    Added comments

--- replay.py
+++ replay.py
@@ -2,12 +2,15 @@ import math
 import wav_key_derive as w
 
 def replayAndPredict(getKey, wavfile, plaintext):
+    #Gets the key streams
     keystream1 = w.wav_key_derive(wavfile, 1 , plaintext)
     keystream2 = w.wav_key_derive(wavfile, 1 , plaintext)
 
+    #Checks to see if the key streams are equal
     isReplayVul = (keystream1 == keystream2)
     replayStatus = "Replay Risk" if isReplayVul else "Secure"
 
+    #Gets the bits and see if they have a correlation
     keystream1Bytes = [ord(c) for c in keystream1] if isinstance(keystream1, str) else keystream1
     keystream2Bytes = [ord(c) for c in keystream2] if isinstance(keystream2, str) else keystream2
 
