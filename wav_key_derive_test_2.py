23778afb465acfe6bf9ac3537f3bd69400022ce9
baa0054 <baa0054@auburn.edu>
2026-04-11 21:12:41 -0500

    Update wav_key_derive_test_2.py
    Adding the test

--- wav_key_derive_test_2.py
+++ wav_key_derive_test_2.py
@@ -1,11 +1,20 @@
-import time
-import wav_key_derive
 import wave
+import entropy_test as et
+import wav_key_derive
+import plaintextAttack as pta
+import replay as re
+import time
 """
 This Python script measures the time (in seconds) to generate a key from each WAV file from 1 audio frame to its total 
 number of audio frames.
 Run: python3 wav_key_derive_test_2
 """
+
+FILE_W = 25
+TEXT_W = 20
+STAT_W = 10
+PTA_W  = 15
+PRE_W  = 12
 
 """Variable for the number of frames"""
 number_of_frames : int
@@ -16,6 +25,10 @@ def wav_key_derive_test_2():
 
     files = ['wav_files/Moderato.wav', 'wav_files/sample-1.wav', 'wav_files/sample-2.wav',
              'wav_files/sample-3.wav', 'wav_files/sample-4.wav', 'wav_files/sample-5.wav']
+    
+    simResults = []
+    
+    test = "Test plaintext"
 
     for filename in files:
         with wave.open(filename, 'rb') as w:
@@ -35,6 +48,18 @@ def wav_key_derive_test_2():
                 print(f"Number of audio frames: {i}")
                 print(f"Key Length as number of hex bytes: {len(key.split('\\x')) - 1}")
                 print(f"Key Generation Time: {elapsed_time} seconds\n")
+                derived_key = wav_key_derive.wav_key_derive(filename, i)
+
+                # Perform entropy test on the KEYSTREAM
+                testResults = et.randomness_simulations(derived_key, filename, test)
+                replayandpredict = re.replayAndPredict(derived_key, filename, test)
+                recoveredKey = pta.lookForKeyLoop(derived_key)
+                testResults.update({
+                        "replay": replayandpredict['replay'],
+                        "predict": replayandpredict['predictability'],
+                        "pta": recoveredKey
+                    })
+                simResults.append(testResults)
 
         """Key Generation Time with the WAV file's total number of frames."""
         start_time = time.time()
@@ -47,6 +72,15 @@ def wav_key_derive_test_2():
         print(f"Key Generation Time: {elapsed_time} seconds")
         print("----------------------------------------------------------------------------------------------")
         print("----------------------------------------------------------------------------------------------")
+
+
+    print("Simulation Report\n")
+    header = f"{'WAV File':<{FILE_W}} | {'Plaintext':<{TEXT_W}} | {'Status':<{STAT_W}} | {'PTA (Period)':<{PTA_W}} | {'Replay':<{PRE_W}} | {'Predictability':<{PRE_W}}"
+    print(header)
+    for res in simResults:
+         display = ((res['text'][:TEXT_W-3] + "...") if len(res['text']) > TEXT_W else res['text'])
+         print(f"{res['file']:<{FILE_W}} | "f"{display:<{TEXT_W}} | "f"{str(res['status']):<{STAT_W}} | "f"{str(res['pta']):<{PTA_W}} | "f"{str(res['replay']):<{PRE_W}} | "f"{str(res['predict']):<{PRE_W}}")
+
 
 if __name__ == '__main__':
     wav_key_derive_test_2()
